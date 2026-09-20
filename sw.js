aac48a9919f20b8a91a99b3253271877bbbe0e6d
benroman7888 <benr0man@proton.me>
2026-09-20 18:23:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const CACHE="money-planner-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
+const CACHE="money-planner-v2";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
